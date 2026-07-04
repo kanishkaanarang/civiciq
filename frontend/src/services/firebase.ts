45bc31d4807556d5a6586679dcaf8a4bec61cc34
kanishkaanarang <kanishkaanarang@gmail.com>
2026-07-05 04:22:04 +0530

    changes

--- frontend/src/services/firebase.ts
+++ frontend/src/services/firebase.ts
@@ -9,6 +9,7 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
+console.log("Firebase Config:", firebaseConfig);
 
 const app = initializeApp(firebaseConfig);
 
