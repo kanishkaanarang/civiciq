b54ea97c3cb91aa000bf3718f2b83610996ce203
kanishkaanarang <kanishkaanarang@gmail.com>
2026-07-07 18:01:25 +0530

    Fix CORS for geocode endpoint

--- backend/main.py
+++ backend/main.py
@@ -15,7 +15,8 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "*",
+        "http://localhost:5173",
+        "https://civiciq-gamma.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
